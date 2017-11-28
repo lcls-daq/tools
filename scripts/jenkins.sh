d9e01d3ac50b5ab07975be293d0316d1e834e07f
Daniel Damiani <ddamiani@slac.stanford.edu>
2017-11-27 16:24:44 -0800

    jenkins now builds tarballs

--- scripts/jenkins.sh
+++ scripts/jenkins.sh
@@ -7,14 +7,20 @@ if [ ! -d tools ]; then
   exit 1
 fi
 
+buildtype="unknown"
 # check the jenkins node labels
 for label in $NODE_LABELS
 do
   if [ "$label" == "rhel7" ]; then
       tools/scripts/cleanbuild.sh  --no32
+      buildtype="$label"
     elif [ "$label" == "rhel6" ]; then
       tools/scripts/cleanbuild.sh  --no32
+      buildtype="$label"
     elif [ "$label" == "rhel5" ]; then
       tools/scripts/cleanbuild.sh
+      buildtype="$label"
     fi
 done
+
+tools/scripts/tarcreate.sh pdsbuild-pdsapp-${buildtype}-${BUILD_NUMBER}-${SVN_REVISION}.tar.gz
