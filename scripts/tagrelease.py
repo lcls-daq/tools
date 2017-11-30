3751d01d12c76c29a8e8ad349bd4c5b2113ecb9e
Daniel Damiani <ddamiani@slac.stanford.edu>
2017-11-29 16:33:27 -0800

    tagrelease now works with git

--- scripts/tagrelease.py
+++ scripts/tagrelease.py
@@ -4,8 +4,6 @@
 import os
 import subprocess
 import sys
-from tempfile import NamedTemporaryFile, mkdtemp
-from shutil import rmtree
 from optparse import OptionParser
 
 usage = "%prog [options] tag"
@@ -24,10 +22,13 @@ DAQURL='https://pswww.slac.stanford.edu/svn/pdsrepo'
 daq_subdirs = ['pds', 'pdsapp', 'timetool', 'tools']
 ami_subdirs = ['ami', 'tools']
 
+
+def push(pkg):
+    return subprocess.call(['/usr/bin/git push origin '+tag], cwd = pkg, shell = True)
+
 def make_tag(pkg):
-    path=DAQURL+'/'+pkg
-    cmd = ['svn','copy',path+'/trunk',path+'/tags/'+tag,'-m','\"Tag '+tag+'\"']
-    return subprocess.call(cmd)
+    cmd = ['/usr/bin/git tag '+tag+' -m "Tag %s"'%tag]
+    return subprocess.call(cmd, cwd = pkg, shell = True)
 
 def verify_tag(pkg):
     rv = False
@@ -45,12 +46,6 @@ def make_tagd(pkg):
         print 'Warning: directory \'%s\' not found, tagging skipped' % pkg
     return retval
 
-def checkout(tmpdir, project):
-    os.chdir(tmpdir)
-    cmd = '/usr/bin/svn co --depth files %s/%s/tags/%s %s' % (DAQURL, project, tag, project)
-    p = subprocess.Popen([cmd], shell = True, stdout = subprocess.PIPE, close_fds = True)
-    return subprocess.Popen.communicate(p)[0]
-
 def fetch(project_path):
     p = subprocess.Popen(['/usr/bin/git fetch origin'], cwd = project_path, shell = True, stdout = subprocess.PIPE, close_fds = True)
     return subprocess.Popen.communicate(p)[0]
@@ -67,43 +62,13 @@ def get_current_branch(project_path):
     p = subprocess.Popen(['/usr/bin/git rev-parse --abbrev-ref HEAD'], cwd = project_path, shell = True, stdout = subprocess.PIPE, close_fds = True)
     return subprocess.Popen.communicate(p)[0]
 
-def set_externals_property(project_path, newpropvalue, oldtext, newtext):
-    retvalue = 1
-    # write new property to a temporary file
-    tt = NamedTemporaryFile()   # create temporary file with visible name in the file system
-    tt.write(newpropvalue)
-    tt.flush()                  # flush the written data before svn reopens the file for reading
-    rv = subprocess.call(['/usr/bin/svn propset svn:externals -F %s %s' % (tt.name, project_path)], shell=True)
-    tt.close()                  # the temporary file is deleted as soon as it is closed
-    if rv:
-        print 'Error: svn propset failed'
-    else:
-        if subprocess.call(['/usr/bin/svn commit -m \'changed externals from %s to %s\' %s' % (oldtext, newtext, project_path)], shell=True):
-            print 'Error: svn commit failed'
-        else:
-            retvalue = 0
-    return retvalue
+def compare_rev(project_path):
+    p = subprocess.Popen(['/usr/bin/git rev-parse master'], cwd = project_path, shell = True, stdout = subprocess.PIPE, close_fds = True)
+    loc = subprocess.Popen.communicate(p)[0]
+    p = subprocess.Popen(['/usr/bin/git rev-parse origin/master'], cwd = project_path, shell = True, stdout = subprocess.PIPE, close_fds = True)
+    rem = subprocess.Popen.communicate(p)[0]
+    return loc != rem
 
-def update_externals_property(tmpdir, project, oldtext, newtext):
-    retval = 1
-    if not checkout(tmpdir, project):
-        print 'Error: checkout of %s/tags/%s from %s failed' % (project, tag, DAQURL)
-    else:
-        # read old externals property value
-        oldexternals = get_externals_property(tmpdir+'/'+project)
-
-        # create new externals property value based on the old one
-        newexternals = oldexternals.replace(oldtext,newtext)
-
-        if newexternals == oldexternals:
-            print 'Error: failed to replace trunk with tag in svn:externals property'
-        else:
-            # write new externals property value
-            if set_externals_property(tmpdir+'/'+project, newexternals, oldtext, newtext):
-                print 'Error: setting svn:externals property failed'
-            else:
-                retval = 0
-    return retval
 
 if __name__ == '__main__':
 
@@ -120,15 +85,18 @@ if __name__ == '__main__':
 
     if not options.force:
         fail = False
-        # sanity check 1: tag begins with 'V' or 'ami-V'
+        # sanity check 0: tag begins with 'V' or 'ami-V'
         if tag.find('V') and tag.find('ami-V'):
             print 'sanity check failed: tag \'%s\' does not begin with \'V\' or \'ami-V\'' % tag 
             fail = True
 
-        # check for changes from origin
+        # sanity check 1: check that local branches synced with remote
         for dir in dirlist:
             fetch(dir)
             fetch_tags(dir)
+            if compare_rev(dir):
+                print 'sanity check failed: working directory \'%s\' out of sync with remote' % dir
+                fail = True
 
         # sanity check 2: check that current directory and submodules are clean
         for dir in dirlist:
@@ -166,29 +134,41 @@ if __name__ == '__main__':
         print 'tag \'%s\' not applied (dry run)' % tag
         sys.exit(0)
 
-    retval = 1
-    tmpdir = mkdtemp()      # create temporary directory
+    retval = 0
     if tag.find('ami')<0:
-        if make_tag('release'):
+        if make_tag('.'):
+            retval = 1
             print 'Error: tagging release failed'
         else:
             for dir in daq_subdirs:
-                make_tagd(dir)
-            # update externals
-            if update_externals_property(tmpdir, 'release', 'trunk', 'tags/'+tag):
-                print 'Error: updating svn:externals property failed'
+                if make_tagd(dir):
+                    retval = 1
+                    print 'Error: tagging %s failed' % dir
+            # update remote
+            if push('.'):
+                retval = 1
+                print 'Error: pushing release failed'
             else:
-                retval = 0  # success
+                for dir in daq_subdirs:
+                    if push(dir):
+                        retval = 1
+                        print 'Error: pushing %s failed' % dir
+            retval = 0  # success
     else:
-        if make_tag('ami-release'):
+        if make_tag('.'):
+            retval = 1
             print 'Error: tagging ami-release failed'
         else:
             for dir in ami_subdirs:
                 make_tagd(dir)
-            # update externals
-            if update_externals_property(tmpdir, 'ami-release', 'trunk', 'tags/'+tag):
-                print 'Error: updating svn:externals property failed'
+            # update remote
+            if push('.'):
+                retval = 1
+                print 'Error: pushing ami-release failed'
             else:
-                retval = 0  # success
-    rmtree(tmpdir)          # delete temorary directory
+                for dir in ami_subdirs:
+                    if push(dir):
+                        retval = 1
+                        print 'Error: pushing %s failed' % dir
+            retval = 0  # success
     sys.exit(retval)
