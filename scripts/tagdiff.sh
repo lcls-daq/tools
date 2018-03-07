da28720d93979e713900c2ecceb433279cefc804
Daniel Damiani <ddamiani@slac.stanford.edu>
2018-03-07 14:42:15 -0800

    add script for check the diff log between tags

---
+++ scripts/tagdiff.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# check args
+if [ $# -ne 2 ]; then
+  echo "Usage: $0 <old_tag> <new_tag>"
+fi
+
+OLDTAG="${1}"
+NEWTAG="${2}"
+
+DAQ_DIRS="pds pdsapp timetool tools"
+AMI_DIRS="ami tools"
+
+git log "${OLDTAG}".."${NEW_TAG}"
+for DIR in $DAQ_DIRS $AMI_DIRS
+do
+  if [ -d "$DIR" ]; then
+    OLD_DIR="$PWD"
+    cd -- "$DIR"
+    git log "${OLDTAG}".."${NEW_TAG}"
+    cd -- "$OLD_DIR"
+  fi
+done
